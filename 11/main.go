b71f19984989eb55e56ddc2229cfbdabe548fade
fbakly <fouadelbakly130@gmail.com>
2020-12-12 10:20:56 +0100

    Finished day 12

--- 11/main.go
+++ 11/main.go
@@ -38,7 +38,7 @@ func numAdjacent(lines []string, row, col int, recur bool) int {
 	return seen
 }
 
-func part1(seatMap []string) int {
+func solve(seatMap []string, patience int, recur bool) int {
 	numSeats := 0
 	lines := make([]string, len(seatMap))
 	copy(lines, seatMap)
@@ -50,44 +50,12 @@ func part1(seatMap []string) int {
 		for i := 0; i < len(linesCopy); i++ {
 			for j := 0; j < len(linesCopy[i]); j++ {
 				if linesCopy[i][j] == 'L' {
-					if numAdjacent(linesCopy, i, j, false) == 0 {
+					if numAdjacent(linesCopy, i, j, recur) == 0 {
 						lines[i] = lines[i][:j] + "#" + lines[i][j+1:]
 						numSeats++
 					}
 				} else if linesCopy[i][j] == '#' {
-					if numAdjacent(linesCopy, i, j, false) >= 4 {
-						lines[i] = lines[i][:j] + "L" + lines[i][j+1:]
-						numSeats--
-					}
-				}
-			}
-		}
-		if prevNumSeats == numSeats {
-			break
-		}
-	}
-
-	return numSeats
-}
-
-func part2(seatMap []string) int {
-	numSeats := 0
-	lines := make([]string, len(seatMap))
-	copy(lines, seatMap)
-
-	for true {
-		prevNumSeats := numSeats
-		linesCopy := make([]string, len(lines))
-		copy(linesCopy, lines)
-		for i := 0; i < len(linesCopy); i++ {
-			for j := 0; j < len(linesCopy[i]); j++ {
-				if linesCopy[i][j] == 'L' {
-					if numAdjacent(linesCopy, i, j, true) == 0 {
-						lines[i] = lines[i][:j] + "#" + lines[i][j+1:]
-						numSeats++
-					}
-				} else if linesCopy[i][j] == '#' {
-					if numAdjacent(linesCopy, i, j, true) >= 5 {
+					if numAdjacent(linesCopy, i, j, recur) >= patience {
 						lines[i] = lines[i][:j] + "L" + lines[i][j+1:]
 						numSeats--
 					}
@@ -105,6 +73,6 @@ func part2(seatMap []string) int {
 
 func main() {
 	lines := aoc.GetStdin()
-	fmt.Printf("Part 1 solution: %d\n", part1(lines))
-	fmt.Printf("Part 2 solution: %d\n", part2(lines))
+	fmt.Printf("Part 1 solution: %d\n", solve(lines, 4, false))
+	fmt.Printf("Part 2 solution: %d\n", solve(lines, 5, true))
 }
---
+++ 12/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"aoc"
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func move(instruction byte, value int, facing *int, pos []int) {
+
+	switch instruction {
+	case 'N':
+		pos[1] += value
+	case 'S':
+		pos[1] -= value
+	case 'E':
+		pos[0] += value
+	case 'W':
+		pos[0] -= value
+	case 'L':
+		*facing += value
+		*facing %= 360
+	case 'R':
+		*facing -= value
+		if *facing < 0 {
+			*facing += 360
+		}
+	case 'F':
+		switch *facing {
+		case 0:
+			move('E', value, facing, pos)
+		case 90:
+			move('N', value, facing, pos)
+		case 180:
+			move('W', value, facing, pos)
+		case 270:
+			move('S', value, facing, pos)
+		default:
+			fmt.Println(*facing)
+		}
+	default:
+		return
+	}
+
+}
+
+func moveWaypoint(instruction byte, value int, waypointPos, shipPos []int) {
+	switch instruction {
+	case 'N':
+		waypointPos[1] += value
+	case 'S':
+		waypointPos[1] -= value
+	case 'E':
+		waypointPos[0] += value
+	case 'W':
+		waypointPos[0] -= value
+	case 'F':
+		shipPos[0] += (waypointPos[0] * value)
+		shipPos[1] += (waypointPos[1] * value)
+	case 'L':
+		if value == 90 {
+			temp := -waypointPos[1]
+			waypointPos[1] = waypointPos[0]
+			waypointPos[0] = temp
+		} else if value == 180 {
+			for i := 0; i < 2; i++ {
+				moveWaypoint(instruction, 90, waypointPos, shipPos)
+			}
+		} else if value == 270 {
+			for i := 0; i < 3; i++ {
+				moveWaypoint(instruction, 90, waypointPos, shipPos)
+			}
+		} else {
+			fmt.Println(value)
+		}
+	case 'R':
+		if value == 90 {
+			temp := -waypointPos[0]
+			waypointPos[0] = waypointPos[1]
+			waypointPos[1] = temp
+		} else if value == 180 {
+			for i := 0; i < 2; i++ {
+				moveWaypoint(instruction, 90, waypointPos, shipPos)
+			}
+		} else if value == 270 {
+			for i := 0; i < 3; i++ {
+				moveWaypoint(instruction, 90, waypointPos, shipPos)
+			}
+		} else {
+			fmt.Println(value)
+		}
+	default:
+		fmt.Println(instruction)
+	}
+}
+
+func part1(lines []string) int {
+	facing := 0
+	pos := []int{0, 0}
+
+	for _, line := range lines {
+		instruction := line[0]
+		value, _ := strconv.Atoi(line[1:])
+		move(instruction, value, &facing, pos)
+	}
+	return int(math.Abs(float64(pos[0])) + math.Abs(float64(pos[1])))
+}
+
+func part2(lines []string) int {
+	shipPos := []int{0, 0}
+	waypointPos := []int{10, 1}
+
+	for _, line := range lines {
+		instruction := line[0]
+		value, _ := strconv.Atoi(line[1:])
+		moveWaypoint(instruction, value, waypointPos, shipPos)
+	}
+	return int(math.Abs(float64(shipPos[0])) + math.Abs(float64(shipPos[1])))
+}
+
+func main() {
+	lines := aoc.GetStdin()
+	fmt.Printf("Part1 solution: %d\n", part1(lines))
+	fmt.Printf("Part2 solution: %d\n", part2(lines))
+}
